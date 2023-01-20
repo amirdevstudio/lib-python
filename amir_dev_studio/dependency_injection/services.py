b8e173701b233131d8a578c17fa7f17b90791d7e
Amir Sharapov <amir@amirfounder.com>
2023-01-20 00:41:09 -0600

    implemented namespaces

--- amir_dev_studio/dependency_injection/services.py
+++ amir_dev_studio/dependency_injection/services.py
@@ -1,83 +1,109 @@
-from typing import Dict, Any
+from typing import Dict, Any, TypeVar, Type
 
 from amir_dev_studio.dependency_injection.providers import Singleton, Transient, BaseProvider
 
-_provider_container: Dict[Any, BaseProvider] = {}
+
+_ArgumentNotSpecified = object()
+_default_namespace = 'default'
+_provider_container: Dict[str, Dict[Type, BaseProvider]] = {}
+_T = TypeVar('_T')
 
 
-def _add_service_to_container(key, value: BaseProvider):
-    if key in _provider_container:
-        raise ValueError(f'Key {key} already exists in the container')
-    _provider_container[key] = value
+def _add_service_to_container(service_class: Type, provider: BaseProvider, namespace: str = _default_namespace) -> None:
+    if (
+        namespace in _provider_container and
+        service_class in _provider_container[namespace]
+    ):
+        raise ValueError(f'Service {service_class} already exists in the container (namespace: {namespace})')
+
+    if namespace not in _provider_container:
+        _provider_container[namespace] = {}
+
+    _provider_container[namespace][service_class] = provider
 
 
-def get_service(key):
-    if key not in _provider_container:
-        raise ValueError(f'Key {key} does not exist in the container')
-    return _provider_container[key].get_service()
+def get_service(service_class: Type, default: Any = _ArgumentNotSpecified, namespace: str = _default_namespace) -> _T:
+    if (
+        namespace not in _provider_container or
+        service_class not in _provider_container[namespace]
+    ):
+        if default is not _ArgumentNotSpecified:
+            return default
+
+        raise Exception(f"No service was found for the given class: {service_class} (namespace: {namespace})")
+
+    return _provider_container[namespace][service_class].get_service()
 
 
-def add_singleton_service(service_class, service_init_args=None, service_init_kwargs=None):
+def get_singleton_service(service_class: Type[_T], default=_ArgumentNotSpecified) -> _T:
+    return get_service(service_class, default, service_class)
+
+
+def add_singleton_service(
+        service_class,
+        service_init_args: tuple = None,
+        service_init_kwargs: dict = None,
+        namespace: str = _default_namespace,
+):
     _add_service_to_container(
         service_class,
         Singleton(
             service_class,
             service_init_args,
             service_init_kwargs,
-        )
+        ),
+        namespace
     )
 
 
-def add_transient_service(service_class, service_init_args=None, service_init_kwargs=None):
+def add_transient_service(
+        service_class,
+        service_init_args: tuple = None,
+        service_init_kwargs: dict = None,
+        namespace: str = _default_namespace,
+):
     _add_service_to_container(
         service_class,
         Transient(
             service_class,
             service_init_args,
             service_init_kwargs,
-        )
+        ),
+        namespace
     )
 
 
-def add_abstract_transient_service(abstract_class, concrete_class, service_init_args=None, service_init_kwargs=None):
+def add_abstract_transient_service(
+        abstract_class,
+        concrete_class,
+        service_init_args: tuple = None,
+        service_init_kwargs: dict = None,
+        namespace: str = _default_namespace,
+):
     _add_service_to_container(
         abstract_class,
         Transient(
             concrete_class,
             service_init_args,
             service_init_kwargs,
-        )
+        ),
+        namespace
     )
 
 
-def add_abstract_singleton_service(abstract_class, concrete_class, service_init_args=None, service_init_kwargs=None):
+def add_abstract_singleton_service(
+        abstract_class,
+        concrete_class,
+        service_init_args: tuple = None,
+        service_init_kwargs: dict = None,
+        namespace: str = _default_namespace,
+):
     _add_service_to_container(
         abstract_class,
         Singleton(
             concrete_class,
             service_init_args,
             service_init_kwargs,
-        )
-    )
-
-
-def add_named_singleton_service(name, service_class, service_init_args=None, service_init_kwargs=None):
-    _add_service_to_container(
-        name,
-        Singleton(
-            service_class,
-            service_init_args,
-            service_init_kwargs,
-        )
-    )
-
-
-def add_named_transient_service(name, service_class, service_init_args=None, service_init_kwargs=None):
-    _add_service_to_container(
-        name,
-        Transient(
-            service_class,
-            service_init_args,
-            service_init_kwargs
-        )
+        ),
+        namespace
     )
